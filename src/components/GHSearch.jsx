dc2745ac1668a0f06b3cd88754f86ff873538240
William <willspecker@gmail.com>
2020-12-03 14:43:31 +0100

    adds test file for searching gh users refactors search component to class component adds fixture for default search

--- src/components/GHSearch.jsx
+++ src/components/GHSearch.jsx
@@ -1,13 +1,64 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Button, Input } from 'semantic-ui-react'
+import axios from 'axios'
 
-const GHSearch = () => {
-  return (
-    <>
-      <Input type="text" name="search" placeholder="Input GH username"/>
-      <Button name="search">Search</Button>
-    </>
-  )
+class GHSearch extends Component {
+  state = {
+    searchValue: '',
+    gitHubUsers: []
+  }
+
+  setInputValue(e) {
+    this.setState({searchValue: e.target.value})
+  }
+  async performSearch(e) {
+    const q = this.state.searchValue 
+    let response = await axios.get(`https://api.github.com/search/users?q=${q}`)
+    this.setState({ gitHubUsers: response.data.items })
+  }
+  render() {
+    return (
+      <>
+        <Input 
+          type="text" 
+          data-cy="search_input" 
+          placeholder="Input GH username"
+          onChange={(e) => this.setInputValue(e) }/>
+        <Button 
+          onClick={(e)=> this.performSearch(e)}
+          data-cy="search_button">
+          Search
+        </Button>
+        <div data-cy="search_results">
+
+        </div>
+      </>
+    )
+  }
 }
 
 export default GHSearch
+
+// import React from 'react'
+// import { Button, Input } from 'semantic-ui-react'
+
+// const GHSearch = () => {
+//   return (
+//     <>
+//       <Input 
+//         type="text" 
+//         data-cy="search_input" 
+//         placeholder="Input GH username"/>
+//       <Button 
+//         onClick={()=> performSearch()}
+//         data-cy="search_button">
+//         Search
+//       </Button>
+//       <div data-cy="search_results">
+
+//       </div>
+//     </>
+//   )
+// }
+
+// export default GHSearch
