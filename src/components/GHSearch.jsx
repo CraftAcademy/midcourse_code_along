3e9bc60954228983662cff61c894e1287c576741
Thomas <thomas@craftacademy.se>
2020-12-03 14:56:38 +0100

    adds axios
    adds function to perform search and update state with results
    display results on page

--- src/components/GHSearch.jsx
+++ src/components/GHSearch.jsx
@@ -9,29 +9,35 @@ class GHSearch extends Component {
   }
 
   setInputValue(event) {
-    this.setState({searchValue: event.target.value})
+    this.setState({ searchValue: event.target.value })
   }
   async performSearch(event) {
     const q = this.state.searchValue
     let response = await axios.get(`https://api.github.com/search/users?q=${q}`)
-    this.setState({gitHubUsers: response.data.items})
+    this.setState({ gitHubUsers: response.data.items })
   }
   render() {
-    
+    let displayUsers = this.state.gitHubUsers.map(user => {
+      return (
+        <li key={user.id}>{user.login}</li>
+      )
+    })
     return (
       <>
         <Input
           type="text"
           data-cy="search_input"
           placeholder="Input GH username"
-          onChange={event => this.setInputValue(event)}/>
+          onChange={event => this.setInputValue(event)} />
         <Button
           onClick={(event) => this.performSearch(event)}
           data-cy="search_button">
           Search
        </Button>
         <div data-cy='search_results'>
-
+          <ul>
+            {displayUsers}
+          </ul>
         </div>
       </>
     );
